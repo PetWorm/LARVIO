3b27e7207e4d0d3c62986691f867755a4c719dcc
xiaochen <ares43490@126.com>
2020-03-12 21:54:09 +0800

    update run.sh example

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-~/YOUR_PATH/LARVIO/bin/larvio ~/YOUR_PATH_TO_DATASET/MH_01/mav0/imu0/data.csv ~/YOUR_PATH_TO_DATASET/MH_01/mav0/cam0/data.csv ~/YOUR_PATH_TO_DATASET/MH_01/mav0/cam0/data ~/YOUR_PATH/LARVIO/config/euroc.yaml
+~/YOUR_PATH/LARVIO/build/larvio ~/YOUR_PATH_TO_DATASET/MH_01/mav0/imu0/data.csv ~/YOUR_PATH_TO_DATASET/MH_01/mav0/cam0/data.csv ~/YOUR_PATH_TO_DATASET/MH_01/mav0/cam0/data ~/YOUR_PATH/LARVIO/config/euroc.yaml
